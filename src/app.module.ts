0dff3dd9c4f92b9e74f94cd098bb2baea4fe3b64
Adrian Fernandes <boybothere@gmail.com>
2025-08-17 22:05:10 +0530

    feat: Initialize database connection with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/user.entity';
 
 @Module({
   imports: [AuthModule, ConfigModule.forRoot({
     isGlobal: true
-  })],
+  }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: process.env.DB_PASSWORD,
+      database: 'google_oauth_db',
+      entities: [User],
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
